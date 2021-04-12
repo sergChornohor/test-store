9c11365c192ae54e58ba27f88649cfdeceed661c
sergChornohor <chornohor1992@gmail.com>
2021-04-12 13:10:36 +0300

    fix main route

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,7 +11,7 @@ const routes = [
     path: '/',
     name: 'Home',
     component: MainePage,
-    meta: { breadCrumbs: [{ to: 'main', text: '/' }] },
+    meta: { breadCrumbs: [{ to: '/', text: '/' }] },
   },
   {
     path: '/category/',
